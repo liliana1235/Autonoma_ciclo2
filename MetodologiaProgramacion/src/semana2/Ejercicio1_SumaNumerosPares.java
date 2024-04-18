c52c43d558d5f8dd82cd212244d03c3e57d01bb1
liliana1235 <lilianarociocahuayaramos@gmail.com>
2024-04-18 01:31:01 -0500

    agregando entra y salida de variables

--- MetodologiaProgramacion/src/semana2/Ejercicio1_SumaNumerosPares.java
+++ MetodologiaProgramacion/src/semana2/Ejercicio1_SumaNumerosPares.java
@@ -1,12 +1,21 @@
 package semana2;
 
-
+import javax.swing.JOptionPane;
 
 public class Ejercicio1_SumaNumerosPares {
-    
-    public static void main(String[] args){
-        
-        
-    
+
+    public static void main(String[] args) {
+        int ultimoNumero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el ultimo numero"));
+        int sumaTotalPares = 0;
+
+        for (int i = 1; i <= ultimoNumero; i++) {
+            if (i % 2 == 0) {
+                int numeroPar = i;
+                System.out.println("Numero par: " + numeroPar);
+                sumaTotalPares = sumaTotalPares + numeroPar;
+            }
+        }
+
+        System.out.println("La suma total de los numeros pares es: " + sumaTotalPares);
     }
 }
