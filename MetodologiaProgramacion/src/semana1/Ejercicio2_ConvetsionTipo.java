ef698d0172f5bfc23e52fa1264427b4ac70e64c9
liliana1235 <lilianarociocahuayaramos@gmail.com>
2024-04-18 00:50:58 -0500

    agregando conversion de tipo

--- MetodologiaProgramacion/src/semana1/Ejercicio2_ConvetsionTipo.java
+++ MetodologiaProgramacion/src/semana1/Ejercicio2_ConvetsionTipo.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package semana1;
 
-/**
- *
- * @author lcahu
- */
 public class Ejercicio2_ConvetsionTipo {
-    
+
+    public static void main(String[] args) {
+
+        int numero = 10;
+        double numeroDouble = (double) numero;
+        String numeroString = "" + numero;
+
+        System.out.println("El tipo y valor de las Variables son las siguiente: ");
+
+        System.out.println("1. El numero de tipo Entero tiene el valor de: " + numero);
+        System.out.println("2. El numero de tipo Double tiene el valor de: " + numeroDouble);
+        System.out.println("3. El numero de tipo String tiene el valo de: " + numeroString);
+    }
 }
